60778fa7f1f7bd4d3aada3f33991df2449409fe8
Sudeshna <sudeshna50das@gmail.com>
2024-01-16 21:33:58 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,14 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '85fbbe5df6msh4b45055dd101775p14f79bjsn9fc1a0813827',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "f5154d79028ce4cc2196b11290e848b9";
+
