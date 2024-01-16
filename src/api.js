e1b2c510466219fcebe5f1ae2713232381c88777
Sudeshna <71181547+sudeshna-py@users.noreply.github.com>
2024-01-16 22:31:57 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '85fbbe5df6msh4b45055dd101775p14f79bjsn9fc1a0813827',
+		'X-RapidAPI-Key': '',// type your rapid api key here
 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
@@ -10,5 +10,5 @@ export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
 
-export const WEATHER_API_KEY = "f5154d79028ce4cc2196b11290e848b9";
+export const WEATHER_API_KEY = " "; // type your key from openweather API
 
